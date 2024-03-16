d037747dbbaf3925b92c5e0892e296aec7f670db
pulo0 <bartexx13042005@gmail.com>
2024-03-16 01:13:34 +0100

    Fixed the bug and now the reservation is done

--- php_sql_exercise/ClothingStoreDB/reserve.php
+++ php_sql_exercise/ClothingStoreDB/reserve.php
@@ -8,8 +8,9 @@ $db_database = 'clothing_store';
 $con = new mysqli($db_host, $db_user, $db_password, $db_database)
 or die("error encounter on syncing to DB");
 
-$con -> query("update from clothing_store set available='0' where id=$id");
+$con -> query("update clothing_store set available = '0' where id='$id'");
 $con -> close();
+
 
 header("Location: viewData_client.php");
 
