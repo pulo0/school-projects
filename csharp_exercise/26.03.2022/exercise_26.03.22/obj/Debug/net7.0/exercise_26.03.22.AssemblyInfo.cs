b2aa49f3a9b40d30b1cae96d1e84aa7765aed985
pulo0 <bartexx13042005@gmail.com>
2024-10-28 21:37:16 +0100

    feat: added a part of exam done

--- csharp_exercise/26.03.2022/exercise_26.03.22/obj/Debug/net7.0/exercise_26.03.22.AssemblyInfo.cs
+++ csharp_exercise/26.03.2022/exercise_26.03.22/obj/Debug/net7.0/exercise_26.03.22.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercise_26.03.22")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3db26086e7bf1160031e961c705a3db5aea09470")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d0a55fdafc104c3456b75d765d34a302f5e5724")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercise_26.03.22")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercise_26.03.22")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ exams_ee09_e14/ee09-2024-06/results/logowanie.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forum wielbicieli psów</title>
+</head>
+
+<body>
+    <header>
+        <h1>Forum wielbicieli psów</h1>
+    </header>
+    <aside>
+        <img src="pliki4/zad1/obraz.jpg" alt="foksterier">
+    </aside>
+    <section>
+        <h2>Zapisz się</h2>
+        <form action="logowanie.php" method="post">
+            <label for="login">login: </label>
+            <input type="text" name="login">
+            <label for="haslo">haslo: </label>
+            <input type="password" name="haslo">
+            <label for="re-haslo">powtórz hasło: </label>
+            <input type="password" name="re-haslo">
+            <button type="submit">Zapisz</button>
+        </form>
+    </section>
+    <section>
+        <h2>Zapraszamy wszystkich</h2>
+        <ol>
+            <li>właścicieli psów</li>
+            <li>weterynarzy</li>
+            <li>tych, co chcą kupić psa</li>
+            <li>tych, co lubią psy</li>
+        </ol>
+        <a href="regulamin.html">Przeczytaj regulamin forum</a>
+    </section>
+    <footer>
+        <p>Stronę wykonał: </p>
+    </footer>
+</body>
+
+</html>
