542fb15f3646b564b84fecec170ddc18b0ca08a6
pulo0 <bartexx13042005@gmail.com>
2024-10-30 21:57:44 +0100

    chore: added more thingies to exam

--- exams_ee09_e14/ee09-2024-06/results/logowanie.php
+++ exams_ee09_e14/ee09-2024-06/results/logowanie.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forum wielbicieli psów</title>
+    <link rel="stylesheet" href="styl4.css">
 </head>
 
 <body>
