0b5175202815d54e7b98fce24d4435e0a79ec4bb
pulo0 <bartexx13042005@gmail.com>
2024-11-24 18:54:23 +0100

    style: changed CSS in olimpics_php

--- csharp_exercise/02.04.2022/exercise1_02.04.22/obj/Debug/net7.0/exercise1_02.04.22.AssemblyInfo.cs
+++ csharp_exercise/02.04.2022/exercise1_02.04.22/obj/Debug/net7.0/exercise1_02.04.22.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercise1_02.04.22")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fbff08669f577fba170b8926b211e2e210217f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cf7f989819af083a6bbf91de3cea2e41d8b56be")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercise1_02.04.22")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercise1_02.04.22")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
