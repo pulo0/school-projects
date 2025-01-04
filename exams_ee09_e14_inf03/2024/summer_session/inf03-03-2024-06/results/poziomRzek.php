87a8cc9e577f95933f98ab4ebd7e710cc68d285c
pulo0 <bartexx13042005@gmail.com>
2025-01-04 10:09:15 +0100

    chore: restructured the directory in 2024 exams

--- exams_ee09_e14_inf03/2024/summer_session/inf03-03-2024-06/results/poziomRzek.php
+++ exams_ee09_e14_inf03/2024/summer_session/inf03-03-2024-06/results/poziomRzek.php
@@ -1,7 +1,7 @@
 <?php
 $hostname = 'localhost';
-$username = 'test';
-$password = '123';
+$username = 'localhost';
+$password = '';
 $db = 'rzeki';
 $conn = mysqli_connect($hostname, $username, $password, $db) or die(error_reporting());
 ?>
--- exercises/csharp_exercise/02.04.2022/exercise1_02.04.22/obj/Debug/net7.0/exercise1_02.04.22.AssemblyInfo.cs
+++ exercises/csharp_exercise/02.04.2022/exercise1_02.04.22/obj/Debug/net7.0/exercise1_02.04.22.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercise1_02.04.22")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+713bd8e81f6152325741023fc4d6b19b8eff021e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5a1ad257bcacdcb4edd924c22a5e248e6634dd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercise1_02.04.22")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercise1_02.04.22")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
